d1306357489cb3db755750db1a69c4c325f1b1e0
Ubuntu <ubuntu@ip-172-31-14-149.eu-west-2.compute.internal>
2025-03-11 09:58:32 +0000

    fix: prepare env

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -5,82 +5,59 @@ export REPO_DIR="$(realpath "$(dirname "$(realpath "$0")")"/..)"
 ################################################################################
 # Clone TFHE-rs and patch it with Belfort FPGA integration
 
-export TFHERS_DIR="$REPO_DIR/tfhe-rs"
+export TFHERS_DIR="$HOME/tfhe-rs"
 
 export TFHERS_URL=https://github.com/zama-ai/tfhe-rs.git
-export TFHERS_TAG=tfhe-rs-0.8.3
-
+export TFHERS_TAG=tfhe-rs-0.11.3
 git clone --no-checkout $TFHERS_URL $TFHERS_DIR
 
 pushd $TFHERS_DIR
 git checkout tags/$TFHERS_TAG -b $TFHERS_TAG
 git apply $REPO_DIR/belfort.patch
 
-cat <<EOL > "$TFHERS_DIR/tfhe/src/core_crypto/fpga/accelerators/accelerators.rs"
-pub const PARAM_MESSAGE_2_CARRY_2: ContainerParameters = ContainerParameters {
-  fpga_image: "$REPO_DIR/accel.awsxclbin",
-  batch_size: 12,
-  streaming_size: 8,
-  bsk_num_kernels: 1,
-  ksk_num_kernels: 1,
-  bsk_bits: 54,
-  bsk_frac_bits: 46,
-  ksk_bits: 34,
-};
-
-pub const DEFAULT_PARAMETERS_KS_PBS: ContainerParameters = ContainerParameters {
-  fpga_image: "/path/to/boolean.awsxclbin",
-  batch_size: 1,
-  streaming_size: 1,
-  bsk_num_kernels: 1,
-  ksk_num_kernels: 1,
-  bsk_bits: 1,
-  bsk_frac_bits: 1,
-  ksk_bits: 1,
-};
-EOL
-
-git add .
-git checkout -b "Belfort-Release"
-git commit -m "Belfort Release"
-popd
+# git add .
+# git checkout -b "Belfort-Release"
+# git commit -m "Belfort Release"
 
 ################################################################################
 # Install Rust
 
-export ENV_DIR="$REPO_DIR/.env"
-export RUSTUP_HOME=$ENV_DIR/rust/rustup
-export CARGO_HOME=$ENV_DIR/rust/cargo
+# export RUST_SETUP_SH="$REPO_DIR/scripts/rust_setup.sh"
 
-export RUST_SETUP_SH="$REPO_DIR/scripts/rust_setup.sh"
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > $RUST_SETUP_SH
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > $RUST_SETUP_SH
+# chmod +x $RUST_SETUP_SH
+# $RUST_SETUP_SH -y --no-modify-path
+# rm -f $RUST_SETUP_SH
 
-chmod +x $RUST_SETUP_SH
-$RUST_SETUP_SH -y --no-modify-path
-rm -f $RUST_SETUP_SH
+# source "$CARGO_HOME/env"
 
-source "$CARGO_HOME/env"
+# rustup toolchain install nightly-x86_64-unknown-linux-gnu
+# rustup default nightly
 
-rustup toolchain install nightly-x86_64-unknown-linux-gnu
-rustup default nightly
-
-pushd $TFHERS_DIR
 make install_rs_check_toolchain
 make install_rs_build_toolchain
 popd
 
+
+# Program FPGA
+/tools/belfort_v0.3/interface/fpga.py --program && source /tools/belfort_v0.3/interface/setup.sh
+
 ################################################################################
 # Create setup file
 
-export ENV_SETUP_SH="$REPO_DIR/scripts/source_env.sh"
+# export ENV_DIR="$REPO_DIR/.env"
+# export RUSTUP_HOME=$ENV_DIR/rust/rustup
+# export CARGO_HOME=$ENV_DIR/rust/cargo
 
-echo ""
-echo "export RUSTUP_HOME=$RUSTUP_HOME" >> $ENV_SETUP_SH
-echo "export CARGO_HOME=$CARGO_HOME" >> $ENV_SETUP_SH
-echo "export RUST_LOG=warn" >> $ENV_SETUP_SH
-echo ""
-echo "source $CARGO_HOME/env" >> $ENV_SETUP_SH
-echo ""
+# export ENV_SETUP_SH="$REPO_DIR/scripts/source_env.sh"
 
-chmod +x $ENV_SETUP_SH
+# echo ""
+# echo "export RUSTUP_HOME=$RUSTUP_HOME" >> $ENV_SETUP_SH
+# echo "export CARGO_HOME=$CARGO_HOME" >> $ENV_SETUP_SH
+# echo "export RUST_LOG=warn" >> $ENV_SETUP_SH
+# echo ""
+# echo "source $CARGO_HOME/env" >> $ENV_SETUP_SH
+# echo ""
+
+# chmod +x $ENV_SETUP_SH
