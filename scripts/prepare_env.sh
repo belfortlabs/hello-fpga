e42d3fb329e427a32da13028faa2df886c69aae4
Tuditi <45079109+Tuditi@users.noreply.github.com>
2025-06-27 16:25:03 +0200

    feat: improve script (#4)

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -6,28 +6,44 @@ export REPO_DIR="$(realpath "$(dirname "$(realpath "$0")")"/..)"
 # Clone TFHE-rs and patch it with Belfort FPGA integration
 
 export TFHERS_DIR="$HOME/tfhe-rs"
-
-echo "============="
-echo "Clone TFHE-rs"
-
-export TFHERS_URL=https://github.com/zama-ai/tfhe-rs.git
 export TFHERS_TAG=tfhe-rs-0.11.3
-git clone --no-checkout $TFHERS_URL $TFHERS_DIR
+export TFHERS_URL=https://github.com/zama-ai/tfhe-rs.git
+export PATCH_COMMIT_MSG="Belfort Patch Applied"
 
-pushd $TFHERS_DIR
-echo "==========================================="
-echo "Patch TFHE-rs for Belfort FPGA acceleration"
+separator() {
+    printf "\n=========================================================\n\n"
+}
 
-git checkout tags/$TFHERS_TAG -b $TFHERS_TAG
-git apply $REPO_DIR/belfort.patch
+if [ -d "$TFHERS_DIR" ]; then 
+    echo "Stash changes and set ZAMA GitHub as the origin"
+    pushd $TFHERS_DIR
+    git stash -m "Stashed changes"
+    git remote set-url origin $TFHERS_URL
+else 
+    echo "Fresh Clone of TFHE-rs"
+    git clone --no-checkout $TFHERS_URL $TFHERS_DIR
+    pushd $TFHERS_DIR
+fi
 
-echo "================================="
-echo "Group all changes into one commit"
+separator
+echo "Checkout TFHE-rs for Belfort FPGA acceleration"
+PATCH_COMMIT=$(git log --grep="$PATCH_COMMIT_MSG" --format="%H" | head -n 1)
 
-git add .
-git commit -m "Belfort Release"
+if [ -n "$PATCH_COMMIT" ]; then
+    git checkout $PATCH_COMMIT
+else
+    git checkout tags/$TFHERS_TAG -B $TFHERS_TAG
+    
+    echo "Applying Belfort patch..."
+    git apply $REPO_DIR/belfort.patch
 
-echo "====================="
+    separator
+    git add .
+    echo "Group all changes into one commit"
+    git commit -m "$PATCH_COMMIT_MSG"
+fi
+
+separator
 echo "Update rust if needed"
 
 make install_rs_check_toolchain
