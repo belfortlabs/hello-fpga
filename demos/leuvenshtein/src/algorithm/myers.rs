3c5d9b7492b92730cca515115bcab08fd9ead7c4
Furkan Turan <furkan.turan@esat.kuleuven.be>
2025-07-29 19:10:50 +0200

    Add MIT license header to src files

--- demos/leuvenshtein/src/algorithm/myers.rs
+++ demos/leuvenshtein/src/algorithm/myers.rs
@@ -1,3 +1,28 @@
+/*
+* MIT License
+*
+* Copyright (c) 2025 KU Leuven - COSIC
+* Author: Wouter Legiest
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 use std::mem;
 
 use crate::data;
@@ -348,8 +373,7 @@ pub fn process_enc_query_enc_db(
                 let key12 =
                     unchecked_add_packed(&sks, key1.iter().collect(), eq2_lut.iter().collect());
 
-                let key =
-                    unchecked_add_packed(&sks, key12.iter().collect(), v1.iter().collect());
+                let key = unchecked_add_packed(&sks, key12.iter().collect(), v1.iter().collect());
 
                 let mut ct_res = Vec::new();
 
--- demos/leuvenshtein/src/enc_struct.rs
+++ demos/leuvenshtein/src/enc_struct.rs
@@ -1,32 +1,57 @@
+/*
+* MIT License
+*
+* Copyright (c) 2025 KU Leuven - COSIC
+* Author: Wouter Legiest
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 use std::collections::HashMap;
 use std::time::Instant;
 use tfhe::core_crypto::fpga::lookup_vector::LookupVector;
-use tfhe::shortint::prelude::*;
 use tfhe::integer::fpga::BelfortServerKey;
+use tfhe::shortint::prelude::*;
 
 // Struct to maintain the state of the complete application
 pub struct EncStruct<'a> {
-  pub input: String,
-  pub query: String,
-  pub max_factor: usize,
-  pub db_size: usize,
-  pub th: usize,
-  pub time: Instant,
-  pub q_enc: Vec<tfhe::shortint::Ciphertext>,
-  pub q2_enc: Vec<tfhe::shortint::Ciphertext>,
-  pub db_enc_matrix: Vec<Vec<tfhe::shortint::Ciphertext>>,
-  pub db1_enc_matrix: Vec<Vec<tfhe::shortint::Ciphertext>>,
-  pub db_enc_map: HashMap<usize, HashMap<char, Vec<tfhe::shortint::Ciphertext>>>,
-  pub sks: ServerKey,
-  pub cks: ClientKey,
-  pub fpga_key: &'a mut BelfortServerKey,
-  pub one_enc_vec: Vec<tfhe::shortint::Ciphertext>,
-  pub v_matrices: Vec<Vec<Vec<tfhe::shortint::Ciphertext>>>,
-  pub h_matrices: Vec<Vec<Vec<tfhe::shortint::Ciphertext>>>,
-  pub lut_min_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
-  pub lut_1eq_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
-  pub lut_eq_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
-  pub lut_min_vec_fpga: Vec<LookupVector>,
-  pub lut_1eq_vec_fpga: Vec<LookupVector>,
-  pub lut_eq_vec_fpga: Vec<LookupVector>
+    pub input: String,
+    pub query: String,
+    pub max_factor: usize,
+    pub db_size: usize,
+    pub th: usize,
+    pub time: Instant,
+    pub q_enc: Vec<tfhe::shortint::Ciphertext>,
+    pub q2_enc: Vec<tfhe::shortint::Ciphertext>,
+    pub db_enc_matrix: Vec<Vec<tfhe::shortint::Ciphertext>>,
+    pub db1_enc_matrix: Vec<Vec<tfhe::shortint::Ciphertext>>,
+    pub db_enc_map: HashMap<usize, HashMap<char, Vec<tfhe::shortint::Ciphertext>>>,
+    pub sks: ServerKey,
+    pub cks: ClientKey,
+    pub fpga_key: &'a mut BelfortServerKey,
+    pub one_enc_vec: Vec<tfhe::shortint::Ciphertext>,
+    pub v_matrices: Vec<Vec<Vec<tfhe::shortint::Ciphertext>>>,
+    pub h_matrices: Vec<Vec<Vec<tfhe::shortint::Ciphertext>>>,
+    pub lut_min_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
+    pub lut_1eq_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
+    pub lut_eq_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
+    pub lut_min_vec_fpga: Vec<LookupVector>,
+    pub lut_1eq_vec_fpga: Vec<LookupVector>,
+    pub lut_eq_vec_fpga: Vec<LookupVector>,
 }
