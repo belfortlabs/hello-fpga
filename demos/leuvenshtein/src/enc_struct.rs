080085625a33d1010b225c4eab29d3dd2fdac744
WoutLegiest <wouter.legiest@me.com>
2025-08-04 13:44:18 +0200

    Update UI and CPU support Leuvenshtein (#14)
    Added a wait screen on startup to have a visual during the precomputation
    Added a complete CPU support; program can be ran without the fpga flag (will not have fpga acceleration ofcourse)
    
    Authored-by: WoLe <wouter.legiest@esat.kuleuven.be>

--- demos/leuvenshtein/src/enc_struct.rs
+++ demos/leuvenshtein/src/enc_struct.rs
@@ -10,10 +10,8 @@
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
-
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,13 +21,47 @@
 * SOFTWARE.
 */
 
+
+
 use std::collections::HashMap;
 use std::time::Instant;
 use tfhe::core_crypto::fpga::lookup_vector::LookupVector;
-use tfhe::integer::fpga::BelfortServerKey;
 use tfhe::shortint::prelude::*;
 
+#[cfg(feature = "fpga")]
+use tfhe::integer::fpga::BelfortServerKey;
+
 // Struct to maintain the state of the complete application
+
+#[cfg(not(feature = "fpga"))]
+pub struct EncStruct {
+    pub input: String,
+    pub query: String,
+    pub max_factor: usize,
+    pub db_size: usize,
+    pub th: usize,
+    pub time: Instant,
+    pub q_enc: Vec<tfhe::shortint::Ciphertext>,
+    pub q2_enc: Vec<tfhe::shortint::Ciphertext>,
+    pub db_enc_matrix: Vec<Vec<tfhe::shortint::Ciphertext>>,
+    pub db1_enc_matrix: Vec<Vec<tfhe::shortint::Ciphertext>>,
+    pub db_enc_map: HashMap<usize, HashMap<char, Vec<tfhe::shortint::Ciphertext>>>,
+    pub sks: ServerKey,
+    pub cks: ClientKey,
+    #[cfg(feature = "fpga")]
+    pub fpga_key: &'static mut BelfortServerKey,
+    pub one_enc_vec: Vec<tfhe::shortint::Ciphertext>,
+    pub v_matrices: Vec<Vec<Vec<tfhe::shortint::Ciphertext>>>,
+    pub h_matrices: Vec<Vec<Vec<tfhe::shortint::Ciphertext>>>,
+    pub lut_min_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
+    pub lut_1eq_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
+    pub lut_eq_vec_sw: Vec<tfhe::shortint::server_key::LookupTable<Vec<u64>>>,
+    pub lut_min_vec_fpga: Vec<LookupVector>,
+    pub lut_1eq_vec_fpga: Vec<LookupVector>,
+    pub lut_eq_vec_fpga: Vec<LookupVector>,
+}
+
+#[cfg(feature = "fpga")]
 pub struct EncStruct<'a> {
     pub input: String,
     pub query: String,
