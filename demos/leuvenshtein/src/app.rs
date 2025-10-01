cbf97efefc1a6cfca870548913778628fe9a2f5a
Beren Aydoğan <beren.aydogan@yahoo.com>
2025-10-01 10:31:04 +0000

    fix: make features conditional

--- demos/leuvenshtein/src/app.rs
+++ demos/leuvenshtein/src/app.rs
@@ -609,17 +609,17 @@ impl App {
         let time_string = format!("{:.5}", sec);
 
         let comment = if fpga_enable & enc_struct.input.starts_with("p:") {
-            #[cfg(not(feature = "emulate_fpga"))]
-            "plaintext query and FPGA Acceleration".to_owned()
-
-            #[cfg(feature = "emulate_fpga")]
-            "plaintext query and FPGA Emulation".to_owned()
+            if cfg!(feature = "emulate_fpga") {
+                 "plaintext query and FPGA Emulation".to_owned()
+            } else {
+                "plaintext query and FPGA Acceleration".to_owned()
+            }
         } else if fpga_enable {
-            #[cfg(not(feature = "emulate_fpga"))]
-            "FPGA Acceleration".to_owned()
-
-            #[cfg(feature = "emulate_fpga")]
-            "FPGA Emulation".to_owned()
+            if cfg!(feature = "emulate_fpga") {
+                "FPGA Emulation".to_owned()
+            } else {
+                "FPGA Acceleration".to_owned()
+            }
         } else if enc_struct.input.starts_with("p:") {
             "plaintext query".to_owned()
         } else {
