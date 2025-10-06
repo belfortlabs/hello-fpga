44ee3841a0f47bbc2195f616df6caeff86baca42
Beren Aydoğan <beren.aydogan@yahoo.com>
2025-10-06 07:44:44 +0000

    refactor: remove the unnecessary compile flag

--- demos/erc20/src/main.rs
+++ demos/erc20/src/main.rs
@@ -24,16 +24,12 @@ const NAMES: &[&str] = &[
     "Victor", "Wendy", "Xavier", "Yvonne", "Zara",
 ];
 
-#[cfg(not(feature = "emulate_fpga"))]
 const INSTRUCTIONS: &[&str] = &[
-    "Press 'f' to switch to FPGA execution",
-    "Press 'c' to switch to CPU execution",
-    "Press 'q' to quit",
-];
-
-#[cfg(feature = "emulate_fpga")]
-const INSTRUCTIONS: &[&str] = &[
-    "Press 'f' to switch to FPGA emulation",
+    if cfg!(feature = "emulate_fpga") {
+        "Press 'f' to switch to FPGA emulation"
+    } else {
+        "Press 'f' to switch to FPGA execution"
+    },
     "Press 'c' to switch to CPU execution",
     "Press 'q' to quit",
 ];
