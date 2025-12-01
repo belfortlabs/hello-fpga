9513ca96694238abb0fb0e846299e470668ff18b
Tuditi <david.detroch@belfort.eu>
2025-12-01 10:23:02 +0000

    feat: use default home locations

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -47,9 +47,8 @@ fi
 separator
 echo "Update rust if needed"
 
-export ENV_DIR="$HOME/.cargo"
-export RUSTUP_HOME=$ENV_DIR/rustup
-export CARGO_HOME=$ENV_DIR/cargo
+export RUSTUP_HOME="$HOME/.rustup"
+export CARGO_HOME="$HOME/.cargo"
 
 export RUST_SETUP_SH="$REPO_DIR/scripts/rust_setup.sh"
 
